9f3f6f21936193d6e721ce9c6b904c9fef4c2cbd
RicToni <ricardotonholli@gmail.com>
2025-07-04 23:17:56 -0300

    Rota PUT de tags finalizado

--- src/routes/tags.routes.ts
+++ src/routes/tags.routes.ts
@@ -1,13 +1,14 @@
 import { Router, Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 import { createTagSchema } from '../utils/Schemas/createTagSchema';
+import { updateTagSchema } from '../utils/Schemas/updateTagSchema';
 import { Tag } from '../models/tags.model';
 import { mockTags } from '../repository/tags';
-import {HttpStatusCodes} from '../utils/HttpStatusCode';
+import { HttpStatusCodes } from '../utils/HttpStatusCode';
 
 const router = Router();
 
-router.post('./', createTagSchema,  async (req: Request<{}, {}, Tag>, res: Response): Promise<void>  => {
+router.post('/', createTagSchema,  async (req: Request<{}, {}, Tag>, res: Response): Promise<void>  => {
     const errors = validationResult(req);
     if (!errors.isEmpty){
         res.status(HttpStatusCodes.BAD_REQUEST).json({errors: errors.array()});
@@ -28,3 +29,33 @@ router.post('./', createTagSchema,  async (req: Request<{}, {}, Tag>, res: Respo
     res.status(HttpStatusCodes.CREATED).json(newTag);
 })
 
+router.put('/:id', updateTagSchema, async (req: Request< {id: string}, {}, Partial<Tag>>, res: Response): Promise<void> => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty){
+        res.status(HttpStatusCodes.BAD_REQUEST).json({errors: errors.array()});
+        return;
+    }
+
+    const {id} = req.params;
+    const { name, priority } = req.body;
+
+    const parseId = parseInt(id);
+    if (isNaN(parseId)) {
+        res.status(HttpStatusCodes.BAD_REQUEST).json({errors: 'ID INVÁLIDO'});
+        return;
+    }
+
+    const tagIndex = mockTags.findIndex(tag => tag.id === parseId);
+    if (tagIndex === -1){
+        res.status(HttpStatusCodes.NOT_FOUND).json({errors: 'ID NÃO ENCONTRADO'});
+        return;
+    }
+
+    if (name) mockTags[tagIndex].name = name;
+    if (priority) mockTags[tagIndex].priority = priority;
+    mockTags[tagIndex].updatedAt = new Date();
+
+    res.status(HttpStatusCodes.OK).json(mockTags[tagIndex]);
+})
+
+export default router;
